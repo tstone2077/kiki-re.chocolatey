eda6f897d801483caaaad5d2b9269c933819dfa8
Thurston Stone <tstone2077@gmail.com>
2014-12-13 20:48:35 -0600

    Changed the author to not include an email address...
    because that's how the Chocolatey gods want it.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,5 +1,4 @@
-﻿Import-Module Join-Path "$(Split-Path -parent $MyInvocation.MyCommand.Definition)" "GenerateBinFile"
-$packageName = 'kiki-re'
+﻿$packageName = 'kiki-re'
 $url = 'https://kiki-re.googlecode.com/files/kiki-0.5.6-standalone.zip'
 
 # download and unpack a zip file
